aac15ee51993dc111f35ee02ad75a65a70204877
JutathipPraw <jutathip.sr@kkumail.com>
2024-09-20 00:11:37 +0700

    add path student

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ function Navbar() {
   const handleCrourseListstd = () => {
     navigate("/courseListstd");
   };
+  const handleStudent = () => {
+    navigate("/Student");
+  };
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/");
@@ -60,6 +63,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
+                onClick={handleStudent}
                 className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-100 "
               >
                 <svg
